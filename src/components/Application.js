697ce178f81a16eb5a801a53d3048b6e0657f7cb
Jay <ttjho@hotmail.com>
2020-10-28 19:53:36 -0600

    refactored useState for days and day into object form, changed the way they are called on now

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "components/DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
@@ -21,28 +21,28 @@ const appointments = [
     }
   },
   {
-    id:3,
-    time:"3pm"
+    id: 3,
+    time: "3pm"
   },
   {
-    id:4,
+    id: 4,
     time: "11am",
     interview: {
       student: "Jay",
       interviewer: {
-        id:2,
+        id: 2,
         name: "Tori Malcolm",
         avatar: "https://i.imgur.com/Nmx0Qxo.png"
       }
     }
   },
   {
-    id:5,
+    id: 5,
     time: "5pm",
     interview: {
       student: "Sena",
       interviewer: {
-        id:3,
+        id: 3,
         name: "Mildred Nazir",
         avatar: "https://i.imgur.com/T2WwVfS.png"
       }
@@ -53,37 +53,48 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [days,SetDays] =useState([]);
-  const [day,setDay] = useState("Monday");
-  
+  // const [days,SetDays] =useState([]);
+  // const [day,setDay] = useState("Monday");
+  const [state, setState] = useState(
+    {
+      day: "Monday",
+      days: []
+    });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days =>setState({...state, days})
 
   useEffect(() => {
     axios.get("/api/days").then((response) => {
-      // console.log(response)
-      SetDays(response.data)
-    })
-  },[])
+      setState({...state, days: response.date})
+      
+    });
+  }, []);
 
 
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu"><DayList days={days} day={day} handleSetDay={setDay}  />
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={state.days}
+            day={state.day}
+            handleSetDay={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-      {appointments.map((appointment) => {
+        {appointments.map((appointment) => {
           return <Appointment time={props.time} {...appointment} />;
         })}
         <Appointment key="last" time="5pm" />
