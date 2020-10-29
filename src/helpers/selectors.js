641e4fb5ba32e9f9211cd99eb78a7ebe1f00783e
Jay <ttjho@hotmail.com>
2020-10-29 14:24:12 -0600

    created getInterview helper function and test

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,25 +1,36 @@
-export function getAppointmentsForDay (state, day) {
+export function getAppointmentsForDay(state, day) {
   const appointmentArray = [];
   const days = state.days;
-  
+
   const targetDay = days.find(item => {
-    return item.name === day
-  })
-  
-  if(!days.find(item => item.name === day )) {
+    return item.name === day;
+  });
+
+  if (!days.find(item => item.name === day)) {
     return appointmentArray;
   }
-  
-  // const filteredDays = state.days.filter(targetDay => targetDay.name === day);
-  // const targetDayAppointment = filteredDays[0].appointments;
-   
 
   for (let key in state.appointments) {
-    if(targetDay.appointments.includes(Number(key))) {
-      
-      appointmentArray.push(state.appointments[key])
+    if (targetDay.appointments.includes(Number(key))) {
+
+      appointmentArray.push(state.appointments[key]);
     }
   }
- 
+
   return appointmentArray;
+}
+
+export function getInterview(state, interview) {
+  
+  if(!interview) {
+    return null
+  }
+  
+  const interviewInfo = {
+    "student": interview.student,
+    "interviewer": state.interviewers[interview.interviewer]
+  }
+
+return interviewInfo;
+  
 }
