675bbebaaf306efc256a8f75e80799780e4bedd1
Jay <ttjho@hotmail.com>
2020-10-29 20:03:14 -0600

    added useVisualMode into index.js, added EMPTY, SHOW, CREATE MODES

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,16 +1,37 @@
 import React from "react";
 import "./styles.scss";
+
+import Form from "./Form";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode.js";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 export default function Appointment (props) {
+  const { mode, transition, back } = useVisualMode (
+    props.interview ? SHOW : EMPTY
+  );
 
 return(
 <>
 <Header time={props.time} id={props.id}/>
 <article className="appointment"></article>
-{props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/>}
+{mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+
+{mode === CREATE && <Form interviewers={[]}/>}
+{mode === SHOW && (
+  <Show
+    student={props.interview.student}
+    interviewer={props.interview.interviewer}
+  />
+)}
 </>
 )
 
 };
+
+{/* {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/>} */}
