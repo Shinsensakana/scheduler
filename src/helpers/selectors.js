0ee411daf01a0ea3ce0a5f55072e235598a14bb5
Jay <ttjho@hotmail.com>
2020-10-29 21:08:05 -0600

    added getInterviewersForDay function and tests for function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,26 @@
+export function getInterviewersForDay(state, day) {
+  const interviewersArray = [];
+  const days = state.days;
+
+  const targetDay = days.find(item => {
+    return item.name === day;
+  });
+
+  if (!days.find(item => item.name === day) || state.days.length === 0) {
+    return interviewersArray;
+  }
+ 
+  targetDay.interviewers.map((interviewerId) => {
+    interviewersArray.push(state.interviewers[interviewerId])
+  })
+  
+  
+  
+  return interviewersArray;
+}
+
+
+
 export function getAppointmentsForDay(state, day) {
   const appointmentArray = [];
   const days = state.days;
