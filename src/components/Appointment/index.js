f4aff83d4f1747fc86aa076450abee2adce6e0a9
Jay <ttjho@hotmail.com>
2020-10-30 14:29:00 -0600

    added save function, and passed it on as props to FORM component and connected bookInterview function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -12,13 +12,28 @@ import useVisualMode from "hooks/useVisualMode.js";
 
 export default function Appointment(props) {
 
+  console.log("props in appointment", props);
+  // console.log("student in appointment", props.interview)
+  // console.log("interviewer in appointment", props.interview.interviewer)
+
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVE = "SAVE";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+
+    props.bookInterview(props.id, interview);
+    transition(SHOW);
+  }
 
   return (
     <>
@@ -26,13 +41,21 @@ export default function Appointment(props) {
       <article className="appointment"></article>
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
 
-      {mode === CREATE && <Form interviewers={props.interviewers} onCancel={() => { return back(EMPTY); }} />}
+      {mode === CREATE &&
+        <Form
+          interviewers={props.interviewers}
+          onSave={save}
+          onCancel={() => { return back(EMPTY); }}
+          id={props.id}
+        />
+      }
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
       )}
+
     </>
   );
 
