66afbb54ce2efe110bb4fd5a473453209ea5f224
Jay <ttjho@hotmail.com>
2020-11-02 10:23:36 -0700

    add prop-types library dev dependency and validated interviewers props in InterviewerList.js

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,14 +1,16 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
 
 return(
 <section className="interviewers">
+
+  
   <h4 className="interviewers__header text--light">Interviewer</h4>
-  
-  
+    
   <ul className="interviewers__list">{props.interviewers.map(interviewer =>{
     
     
@@ -26,3 +28,7 @@ return(
 </section>
 );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
